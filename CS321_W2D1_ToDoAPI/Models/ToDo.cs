6d1e0614e5e08806016896fcb27ffd5917571ed5
Matt Cameron <cameron.matt@gmail.com>
2019-06-09 19:49:40 -0500

    initial commit

---
+++ CS321_W2D1_ToDoAPI/Models/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS321_W2D2_ToDoAPI.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public DateTime DueDate { get; set; }
+    }
+}
---
+++ CS321_W2D1_ToDoAPI/Services/IToDoService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CS321_W2D2_ToDoAPI.Models;
+
+namespace CS321_W2D2_ToDoAPI.Services
+{
+    public interface IToDoService
+    {
+        // CRUDL - create (add), read (get), update, delete (remove), list
+
+        // create
+        ToDo Add(ToDo todo); 
+        // read
+        ToDo Get(int id);
+        // update
+        ToDo Update(ToDo todo); 
+        // delete
+        void Remove(ToDo todo);
+        // list
+        IEnumerable<ToDo> GetAll();
+    }
+}
