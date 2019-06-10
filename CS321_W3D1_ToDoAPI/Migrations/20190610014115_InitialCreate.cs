d21a3908be49337979f5b54aa70a6151f1b92280
Matt Cameron <cameron.matt@gmail.com>
2019-06-09 20:58:41 -0500

    rename project folder

---
+++ CS321_W3D1_ToDoAPI/Migrations/20190610014115_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS321_W3D1_ToDoAPI_EF.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(maxLength: 80, nullable: true),
+                    IsComplete = table.Column<bool>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "DueDate", "IsComplete" },
+                values: new object[] { 1, "Laundry", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "DueDate", "IsComplete" },
+                values: new object[] { 2, "Shopping", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "DueDate", "IsComplete" },
+                values: new object[] { 3, "Mow lawn", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
